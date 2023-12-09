1c88db7cb3f10c0fd2406d144a75326d2c46cb73
sharamall <sharamall@hotmail.com>
2023-12-09 10:40:49 -0500

    day9part1

---
+++ day9part1.rb
@@ -0,0 +1,19 @@
+file = File.open "day9in.txt"
+sum = 0
+file.each_line do |l|
+  vals = l.chomp.split(" ").collect { |it| it.to_i }
+  sequences = [vals]
+  until sequences.last.all? 0
+    next_seq = []
+    sequences.last.each_with_index do |elem, i|
+      next if i == 0
+      next_seq.push(sequences.last[i] - sequences.last[i - 1])
+    end
+    sequences.push next_seq
+  end
+  last_val = 0
+  last_val = sequences.pop[-1] + last_val until sequences.empty?
+  sum += last_val
+end
+file.close
+puts sum
